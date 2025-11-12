81b906e7c673a9b67e44803904480229ef768254
Francis Mukobi <frankhostltd3@gmail.com>
2025-11-13 02:48:58 +0300

    fix: add placeholder login and register routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return redirect('/')->with('info', 'Login functionality coming soon!');
+})->name('login');
+
+Route::get('/register', function () {
+    return redirect('/')->with('info', 'Registration functionality coming soon!');
+})->name('register');
